94c837fb3a1ec92a60609596bc0ddfba34ce595d
asra <asra@netcompany.com>
2017-08-31 16:24:34 +0200

    Dummy and Purchase endpoint

--- PDIS/CESEIT/ServiceGateway/Controllers/ExternalServiceController.cs
+++ PDIS/CESEIT/ServiceGateway/Controllers/ExternalServiceController.cs
@@ -21,6 +21,7 @@ namespace ServiceGateway.Controllers
         private readonly NameValueCollection _users = (NameValueCollection)ConfigurationManager.GetSection("UserSection");
 
 
+
         [System.Web.Http.HttpPost]
         [System.Web.Http.Route("RouteRequest")]
        // [WebInvoke(Method = "POST", UriTemplate = "PostEvent", BodyStyle = WebMessageBodyStyle.Bare, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
@@ -116,7 +117,90 @@ namespace ServiceGateway.Controllers
 
         public async Task<HttpResponseMessage> Purchase(HttpRequestMessage request)
         {
-            throw new NotImplementedException();
+
+            HttpResponseMessage response;
+            //Ensure HTTPS
+            //if (!(request.RequestUri.Scheme == Uri.UriSchemeHttps))
+            //{
+            //    response = new HttpResponseMessage(HttpStatusCode.Forbidden)
+            //    {
+            //        ReasonPhrase = "HTTPS Required",
+            //    };
+            //    return response;
+            //}
+            IEnumerable<string> users;
+            var getUserHeader = request.Headers.TryGetValues("username", out users);
+            if (!getUserHeader)
+            {
+                response = new HttpResponseMessage(HttpStatusCode.Forbidden)
+                {
+                    ReasonPhrase = "'username' header required",
+                };
+                return response;
+            }
+            string storedPassword = "";
+            try
+            {
+                storedPassword = _users.Get(users.First());
+            }
+            catch (Exception)
+            {
+                response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            }
+
+
+            if (storedPassword == "")
+            {
+                response = new HttpResponseMessage(HttpStatusCode.Forbidden)
+                {
+                    ReasonPhrase = "Specified username not recognized",
+                };
+                return response;
+            }
+            IEnumerable<string> passes;
+            var getPassHeader = request.Headers.TryGetValues("password", out passes);
+            if (!getPassHeader)
+            {
+                response = new HttpResponseMessage(HttpStatusCode.Forbidden)
+                {
+                    ReasonPhrase = "'password' header required",
+                };
+                return response;
+            }
+            if (passes.First() != storedPassword)
+            {
+                response = new HttpResponseMessage(HttpStatusCode.Forbidden)
+                {
+                    ReasonPhrase = "Incorrect password",
+                };
+                return response;
+            }
+
+            var jstring = await request.Content.ReadAsStringAsync();
+            OrderRequest requestObject;
+            try
+            {
+                requestObject = JsonConvert.DeserializeObject<OrderRequest>(jstring);
+            }
+            catch (Exception)
+            {
+                response = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                {
+                    ReasonPhrase = "Could not parse JSON",
+                };
+                return response;
+            }
+            //Look-up price and time via data from requestObject
+            //Fill answer
+            OrderResponse answer = new OrderResponse()
+            {
+                Status = 200,
+                Message = "Transaction completed",
+            };
+            response = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            var jsonstring = JsonConvert.SerializeObject(answer);
+            response.Content = new StringContent(jsonstring);
+            return response;
         }
 
     }
