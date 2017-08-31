384efd5dc389535a224166c22652803af6bbd66b
Jens Paulsen <jpa@netcompany.com>
2017-08-31 16:30:34 +0200

    Created DB connection project and UnitTest project

---
+++ PDIS/CESEIT/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PDIS.DataAccess;
+using System.Data.Entity;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 dataAcc = new Class1();
+            dataAcc.testfunction();
+            Assert.AreEqual(1,1);
+        }
+    }
+}
